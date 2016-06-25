5e6cd52c8a48e855f3d2b2a42aacb7164cc042a4
John Wilkinson <jcwilk@gmail.com>
2016-06-25 14:46:11 +0800

    Made logic a bit more robust, better retrying

--- do_auth.rb
+++ do_auth.rb
@@ -6,7 +6,7 @@ Bundler.require(:default)
 
 Capybara::Webkit.configure do |config|
   config.allow_unknown_urls
-  config.timeout = 30
+  config.timeout = 15
   config.ignore_ssl_errors
   config.skip_image_loading
 end
@@ -14,20 +14,25 @@ end
 session = Capybara::Session.new(:webkit)
 
 while(true) do
-  session.visit 'http://craigslist.org'
-  if session.text.include?('missed connections')
-    sleep 10
-  else
-    puts 'redirect detected, getting wifi...'
-    sleep 5
-    begin
-      session.click_on 'Give Me Wi-Fi'
-    rescue Capybara::ElementNotFound
-      puts 'not found, retrying...'
+  begin
+    session.visit 'http://craigslist.org'
+    if session.text.include?('missed connections')
+      sleep 10
+    else
+      puts 'redirect detected, getting wifi...'
       sleep 5
-      session.click_on 'Give Me Wi-Fi'
+      begin
+        session.click_on 'Give Me Wi-Fi'
+      rescue Capybara::ElementNotFound
+        puts 'not found, retrying...'
+        sleep 5
+        session.click_on 'Give Me Wi-Fi'
+      end
+      puts "Button clicked, you\'re welcome!"
+      sleep 5 #let things resolve themselves before looping again
     end
-    sleep 5 #let things resolve themselves before looping again
+  rescue
+    puts "Encountered unexpected error #{$!.inspect}! Retrying..."
   end
 end
 
